2927a735203ba02f578461756210e969b4560714
DESKTOP-67BT1F3\Panda <chrisrberg03@gmail.com>
2024-11-11 19:15:40 +0100

    path fixing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
     plugins: [],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "./index.html"),
+                feed: resolve(__dirname, "./feed/index.html"),
+                profile: resolve(__dirname, "./profile/index.html"),
+            },
+        },
+    },
 });
